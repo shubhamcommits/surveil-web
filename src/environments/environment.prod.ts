8697392ac43ad4acf29f4f68869585be3e04dac7
Shubham Singh <development.shubhamsingh@gmail.com>
2022-12-20 18:59:13 +0530

    updating package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: `https://softrace-server.cyclic.app/`,
+  BASE_URL: `https://softrace-server.cyclic.app`,
   BASE_API_URL: `https://softrace-server.cyclic.app/api/v1`,
 };
